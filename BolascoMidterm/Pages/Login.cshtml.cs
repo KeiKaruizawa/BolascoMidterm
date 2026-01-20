07b486792a249177258364e301f35c977d206085
ACT-STUDENT <ACT-STUDENT@LAB2-PC14>
2026-01-20 09:53:43 +0800

    LOGIN

--- BolascoMidterm/Pages/Login.cshtml.cs
+++ BolascoMidterm/Pages/Login.cshtml.cs
@@ -5,8 +5,37 @@ namespace BolascoMidterm.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        private const string validUsername = "hello@gmail.com";
+        private const string validPassword = "helloworld";
+
         public void OnGet()
         {
+            ErrorMessage = string.Empty;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Username == validUsername && Password == validPassword)
+            {
+                
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+               //
+                ErrorMessage = "Invalid email or password. Please try again.";
+                Username = string.Empty;
+                Password = string.Empty;
+                return Page();
+            }
         }
     }
 }
